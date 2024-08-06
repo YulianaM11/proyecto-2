e5275b25e4aead18a1b8ee61442e19e2aa9a677d
YulianaM11 <rsiceily21@gmail.com>
2024-08-06 16:20:27 -0600

    primer commit

---
+++ 2.py
@@ -0,0 +1,8 @@
+# Paso 1: Definir la función con un parámetro
+def saludar(nombre):  # 'nombre' es el parámetro de la función
+    # Paso 2: Escribir el cuerpo de la función
+    print(f"¡Hola {nombre}!")  # Utilizamos una cadena formateada para incluir el nombre en el saludo
+
+# Paso 3: mostrar la función con un argumento
+saludar("amiga")  # Esto mostrará o imprimira "¡Hola amiga!" en la pantalla
+  
